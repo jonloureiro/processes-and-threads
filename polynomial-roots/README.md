38966223b3872ea40217256601fdd571c57daeef
jonloureiro <jonathan.loureiro.m@gmail.com>
2017-11-29 15:19:50 -0300

    adding readme

---
+++ polynomial-roots/README.md
@@ -0,0 +1,6 @@
+## Exercício 1
+### Enunciado
+Utilizando a linguagem C e o sistema operacional Linux, desenvolva um programa que calcule as raízes, se existirem, de uma equação do segundo grau, sendo que as constantes envolvidas no cálculo deverão ser fornecidas pelo usuário via teclado. Para a resolução da equação, deve-se utilizar três processos além do processo original. O primeiro processo será responsável por calcular o valor do delta e os demais deverão calcular as raízes da equação.
+
+#### Executar
+`` clang -o 1 main.c -lm; ./1``
--- polynomial-roots/main.c
+++ polynomial-roots/main.c
@@ -1,52 +1,57 @@
 #include <stdio.h>
 #include <math.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
 
-int main()
-{
+int main () {
     int i;
-    pid_t pid[3];
-    float delta, x1, x2;
-    int a, b, c;
+    pid_t pid[3], wpid;
+    float delta, raiz, x1, x2, a, b, c;
+    int returnStatusDelta, status;
 
-    scanf("%d %d %d", &a, &b, &c);
+    scanf ("%f %f %f", &a, &b, &c);
 
-    for(i=0;i<3;i++) {
+    for (i = 0; i < 3; i++) {
         pid[i] = fork();
-        if (pid[i] < 0)
-        {
-            perror("fork");
+        if (pid[i] < 0) {
+            perror ("fork");
             return 1;
         }
     }
 
-    if (pid[0] == 0)
-    {
-        //O código aqui dentro será executado no processo filho
-        printf("pid do Filho: %d\n", getpid());
-        delta = b*b-4*a*c;;
+    if (pid[0] == 0) {
+        printf("\n=====\nProcesso %d: Calcular descriminante\n=====\n", getpid());
+        delta = b*b-4*a*c;
+        printf("delta = %.2f\n\n", delta);
+        raiz = sqrt(delta);
+        if (delta < 0)
+            return 1;
+        return 0;
     }
-    if (pid[1] == 0)
-    {
-        //O código aqui dentro será executado no processo filho
-        printf("pid do Filho: %d\n", getpid());
-        x1 = (-b + sqrt(delta))/2;
+    if (pid[1] == 0) {
+        waitpid(pid[0], &returnStatusDelta, 0);
+        if (returnStatusDelta == 1)
+            return 1;
+        else {
+            printf("\n=====\nProcesso %d: Calcular primeira raiz\n=====\n", getpid());
+            x1 = (-b + raiz)/2;
+            printf("%.2f = (-%.2f + %.2f) / 2\n\n", x1, b, raiz);
+            return 0;
+        }
     }
-    if (pid[2] == 0)
-    {
-        //O código aqui dentro será executado no processo filho
-        printf("pid do Filho: %d\n", getpid());
-        x2 = (-b - sqrt(delta))/2;
+    if (pid[2] == 0) {
+        waitpid(pid[0], &returnStatusDelta, 0);
+        if (returnStatusDelta == 1)
+            return 1;
+        printf("\n=====\nProcesso %d: calcular segunda raiz\n=====\n", getpid());
+        x2 = (-b - raiz)/2;
+        printf("%.2f = (-%.2f - %.2f) / 2\n\n", x2, b, raiz);
+        return 0;
     }
-    else
-    {
-        //O código neste trecho será executado no processo pai
+    else {
+        while ((wpid = wait(&status)) > 0);
         printf("pid do Pai: %d\n", getpid());
+        printf("%.2f %.2f %.2f\n", delta, x1, x2 );
     }
-
-
-    printf("Esta regiao sera executada por ambos processos\n\n");
-    printf("%f %f %f\n", delta, x1, x2 );
     return 0;
 }
