d9860c11cb82fb1c1b55601db1a9bb7287dec805
jonloureiro <jonathan.loureiro.m@gmail.com>
2017-12-06 21:16:20 -0300

    adding pipe

--- polynomial-roots/main.c
+++ polynomial-roots/main.c
@@ -1,57 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
 #include <sys/types.h>
 
 int main () {
     int i;
-    pid_t pid[3], wpid;
-    float delta, raiz, x1, x2, a, b, c;
-    int returnStatusDelta, status;
+    int fd[2];
+    pid_t pid[3];
+    float a, b, c;
 
+    printf ("Valores de 'a', 'b' e 'c': ");
     scanf ("%f %f %f", &a, &b, &c);
+
+    //for (i = 0; i < 3; i++) {
+        if (pipe(fd) < 0) {
+            perror("pipe") ;
+            return -1 ;
+        }
+    //}
 
     for (i = 0; i < 3; i++) {
         pid[i] = fork();
         if (pid[i] < 0) {
             perror ("fork");
-            return 1;
-        }
+            exit(1);
+        } else if (pid[i] == 0)
+            break;
     }
 
     if (pid[0] == 0) {
-        printf("\n=====\nProcesso %d: Calcular descriminante\n=====\n", getpid());
+        close(fd[0]); // Bloqueando leitura
+        float delta, raiz;
+        float *p = &raiz;
+
+        printf("\n===== Processo %d: Calcular descriminante =====\n", getpid());
+
         delta = b*b-4*a*c;
-        printf("delta = %.2f\n\n", delta);
-        raiz = sqrt(delta);
+        printf ("Calculando: %.0f = %.0f^2 - 4*%.0f*%.0f\n", delta, b, a, c);
+
         if (delta < 0)
-            return 1;
-        return 0;
+            exit (1);
+        raiz = sqrt(delta);
+
+        write(fd[1], p, sizeof(float));
+        printf("Enviando valor da raiz quadrado do delta: %.2f\n", *p);
+
+        exit (0);
     }
     if (pid[1] == 0) {
-        waitpid(pid[0], &returnStatusDelta, 0);
-        if (returnStatusDelta == 1)
-            return 1;
-        else {
-            printf("\n=====\nProcesso %d: Calcular primeira raiz\n=====\n", getpid());
-            x1 = (-b + raiz)/2;
-            printf("%.2f = (-%.2f + %.2f) / 2\n\n", x1, b, raiz);
-            return 0;
-        }
+        close(fd[1]); // Bloqueando escrita
+        float x;
+        float *raiz;
+
+        printf("\n===== Processo %d: Calcular primeira raiz =====\n", getpid());
+
+        read(fd[0], raiz, sizeof(float));
+        printf("Recebendo valor da raiz: %f\n", *raiz);
+
+        x = (-b + (*raiz))/2;
+        printf("Primeira Raiz = %f\n", x);
+
+        exit(0);
     }
-    if (pid[2] == 0) {
-        waitpid(pid[0], &returnStatusDelta, 0);
-        if (returnStatusDelta == 1)
-            return 1;
-        printf("\n=====\nProcesso %d: calcular segunda raiz\n=====\n", getpid());
+    /*if (pid[2] == 0) {
+        close(fd[0][1]);
+        float x;
+        float *raiz;
+        float *ponteiro = &x;
+
+        printf("\n===== Processo %d: calcular segunda raiz =====\n", getpid());
         x2 = (-b - raiz)/2;
-        printf("%.2f = (-%.2f - %.2f) / 2\n\n", x2, b, raiz);
-        return 0;
-    }
+        exit(0);
+    }*/
     else {
-        while ((wpid = wait(&status)) > 0);
-        printf("pid do Pai: %d\n", getpid());
-        printf("%.2f %.2f %.2f\n", delta, x1, x2 );
+        //printf("\n===== Processo %d: PAI =====\n", getpid());
+        //printf("Raiz 1 = %d\nRaiz 2 = %d\n", x1, x2);
     }
     return 0;
 }
